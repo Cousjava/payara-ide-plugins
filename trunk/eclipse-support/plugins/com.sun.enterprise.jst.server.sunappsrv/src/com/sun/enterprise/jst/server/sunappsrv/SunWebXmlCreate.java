e83bd0dc297b4fe2bf07e746b296b4af1b498a26
ludo <ludo@081d56c4-be80-0410-83e6-e326f4057db8>
2005-12-02 17:28:23 +0000

    Initial contribution for an Eclipse  GlassFish plugin

---
+++ trunk/eclipse-support/plugins/com.sun.enterprise.jst.server.sunappsrv/src/com/sun/enterprise/jst/server/sunappsrv/SunWebXmlCreate.java
@@ -0,0 +1,129 @@
+// <editor-fold defaultstate="collapsed" desc="CDDL Licence">
+/*
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the "License").  You may not use this file except
+ * in compliance with the License.
+ *
+ * You can obtain a copy of the license at
+ * glassfish/bootstrap/legal/CDDLv1.0.txt or
+ * https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * HEADER in each file and include the License file at
+ * glassfish/bootstrap/legal/CDDLv1.0.txt.  If applicable,
+ * add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your
+ * own identifying information: Portions Copyright [yyyy]
+ * [name of copyright owner]
+ */
+// </editor-fold>
+package com.sun.enterprise.jst.server.sunappsrv;
+
+import java.io.InputStream;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.wst.common.componentcore.ComponentCore;
+import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.jst.j2ee.application.internal.operations.J2EEComponentCreationDataModelProvider;
+import org.eclipse.wst.common.componentcore.datamodel.properties.IComponentCreationDataModelProperties;
+import org.eclipse.wst.common.frameworks.datamodel.AbstractDataModelOperation;
+import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IPath;
+
+public class SunWebXmlCreate extends AbstractDataModelOperation  {
+    
+    public SunWebXmlCreate() {
+    }
+    
+    public SunWebXmlCreate(IDataModel model) {
+        super(model);
+    }
+    
+    public IStatus execute(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
+        
+        String runtimeID = model.getStringProperty(J2EEComponentCreationDataModelProvider.RUNTIME_TARGET_ID);
+        //       if (runtimeID != null && runtimeID.startsWith("GlassFish")){
+        execute();
+        //       }
+        
+        return Status.OK_STATUS;
+    }
+    
+    public void execute() {
+        IVirtualComponent comp = ComponentCore.createComponent(getProject());
+        
+//////        String type = J2EEProjectUtilities.getJ2EEProjectType(getProject());
+//////
+//////        if (IModuleConstants.JST_WEB_MODULE.equals(type)) {
+        createDeploymentPlan(getWebDeploymentPlanFile(comp));
+////        }
+    }
+    
+    public static IFile getWebDeploymentPlanFile(IVirtualComponent comp) {
+        IPath deployPlanPath = comp.getRootFolder().getUnderlyingFolder()
+        .getProjectRelativePath().append("WEB-INF").append("sun-web.xml");
+        return comp.getProject().getFile(deployPlanPath);
+    }
+    
+    
+    private static WebAppDeploymentPlan createDeploymentPlan(IFile deployPlanFile) {
+        WebAppDeploymentPlan plan=null;
+        InputStream is=null;
+        try {
+            
+            String moduleName = "ludosdfsdfsdfsdf";
+            plan=new WebAppDeploymentPlan();
+            plan.getSunWebApp().setContextRoot("/"+moduleName);
+            is=plan.getInputStream();
+            deployPlanFile.create(is, false, null);
+        } catch (Exception e) {
+            
+        } finally {
+            try {
+                if (is!=null)
+                    is.close();
+            } catch (Exception e) {
+                // do nothing
+            }
+        }
+        
+        return plan;
+    }
+    
+    
+    
+    public IStatus redo(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
+        return null;
+    }
+    
+    
+    public IStatus undo(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
+        return null;
+    }
+    
+    
+    
+    public String getComponentName() {
+        return model.getProperty(
+                IComponentCreationDataModelProperties.COMPONENT_NAME).toString();
+    }
+    
+    public IProject getProject() {
+        String projectName = model.getProperty(
+                IComponentCreationDataModelProperties.PROJECT_NAME).toString();
+        if (projectName != null) {
+            return ResourcesPlugin.getWorkspace().getRoot().getProject( projectName);
+        }
+        return null;
+    }
+}
---
+++ trunk/eclipse-support/plugins/com.sun.enterprise.jst.server.sunappsrv/src/com/sun/enterprise/jst/server/sunappsrv/WebAppDeploymentPlan.java
@@ -0,0 +1,81 @@
+// <editor-fold defaultstate="collapsed" desc="CDDL Licence">
+/*
+ * The contents of this file are subject to the terms 
+ * of the Common Development and Distribution License 
+ * (the "License").  You may not use this file except 
+ * in compliance with the License.
+ * 
+ * You can obtain a copy of the license at 
+ * glassfish/bootstrap/legal/CDDLv1.0.txt or 
+ * https://glassfish.dev.java.net/public/CDDLv1.0.html. 
+ * See the License for the specific language governing 
+ * permissions and limitations under the License.
+ * 
+ * When distributing Covered Code, include this CDDL 
+ * HEADER in each file and include the License file at 
+ * glassfish/bootstrap/legal/CDDLv1.0.txt.  If applicable, 
+ * add the following below this CDDL HEADER, with the 
+ * fields enclosed by brackets "[]" replaced with your 
+ * own identifying information: Portions Copyright [yyyy] 
+ * [name of copyright owner]
+ */
+// </editor-fold>
+
+
+
+package com.sun.enterprise.jst.server.sunappsrv;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.netbeans.modules.j2ee.sun.dd.api.DDProvider;
+import org.netbeans.modules.j2ee.sun.dd.api.web.SunWebApp;
+
+
+/**
+ * Helper class to access a sun-web-app.xml file.
+ */
+public class WebAppDeploymentPlan {
+    
+    
+    private SunWebApp swa;
+    
+    /**
+     * Loads a deploymentplan.xml from the given URL, and creates one if none exists now
+     *
+     * @param is
+     * @throws Exception if anything goes wrong
+     */
+    public WebAppDeploymentPlan(InputStream is) throws Exception {
+        swa= DDProvider.getDefault().getWebDDRoot(is);
+        
+    }
+    public SunWebApp getSunWebApp(){
+        return swa;
+        
+    }
+    /**
+     * create a deploymentplan.xml
+     *
+     * @throws Exception if anything goes wrong
+     */
+    public WebAppDeploymentPlan() throws Exception {
+        swa = (SunWebApp) DDProvider.getDefault().newGraph(SunWebApp.class);
+        
+    }
+    
+    /**
+     * Returns an input stream for writing to the disk with a local locale.
+     *
+     * @return java.io.InputStream
+     * @throws IOException if anything goes wrong
+     */
+    public InputStream getInputStream() throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        swa.write(out);
+        return new ByteArrayInputStream(out.toByteArray());
+    }
+    
+    
+}
