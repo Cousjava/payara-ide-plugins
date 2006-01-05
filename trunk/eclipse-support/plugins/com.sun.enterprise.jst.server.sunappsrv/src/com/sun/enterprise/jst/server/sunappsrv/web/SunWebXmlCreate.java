4da40bed04fb12e2c382f64b6ad1357972f39eeb
ludo <ludo@081d56c4-be80-0410-83e6-e326f4057db8>
2006-01-05 00:10:51 +0000

    fix license location

--- trunk/eclipse-support/plugins/com.sun.enterprise.jst.server.sunappsrv/src/com/sun/enterprise/jst/server/sunappsrv/web/SunWebXmlCreate.java
+++ trunk/eclipse-support/plugins/com.sun.enterprise.jst.server.sunappsrv/src/com/sun/enterprise/jst/server/sunappsrv/web/SunWebXmlCreate.java
@@ -6,14 +6,14 @@
  * in compliance with the License.
  *
  * You can obtain a copy of the license at
- * glassfish/bootstrap/legal/CDDLv1.0.txt or
+ * glassfishplugins/www/license/CDDLv1.0.txt or
  * https://glassfish.dev.java.net/public/CDDLv1.0.html.
  * See the License for the specific language governing
  * permissions and limitations under the License.
  *
  * When distributing Covered Code, include this CDDL
  * HEADER in each file and include the License file at
- * glassfish/bootstrap/legal/CDDLv1.0.txt.  If applicable,
+ * glassfishplugins/www/license/CDDLv1.0.txt.  If applicable,
  * add the following below this CDDL HEADER, with the
  * fields enclosed by brackets "[]" replaced with your
  * own identifying information: Portions Copyright [yyyy]
