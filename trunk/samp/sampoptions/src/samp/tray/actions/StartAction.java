ef2a79e4aadda5c9c74e19a29f3104e3fa46ffa4
ludo <ludo@081d56c4-be80-0410-83e6-e326f4057db8>
2007-10-08 03:25:22 +0000

    updated ui

---
+++ trunk/samp/sampoptions/src/samp/tray/actions/StartAction.java
@@ -0,0 +1,38 @@
+/*
+ * StartAction.java
+ *
+ * Created on Oct 7, 2007, 7:48:06 PM
+ *
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package samp.tray.actions;
+
+import java.awt.MenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import samp.execution.ServersManager;
+import samp.tray.Tray;
+
+/**
+ *
+ * @author root
+ */
+public class StartAction extends MenuItem {
+
+    public StartAction(final Tray tray) {
+        super(" " + tray.getBundle().getString("LABEL_Start"));
+        addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                ServersManager.StartServers();
+                tray.setIcon("green");
+            }
+        });
+    }
+
+    public boolean isEnabled() {
+        return super.isEnabled();
+    }
+}
