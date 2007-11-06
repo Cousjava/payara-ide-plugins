d8c53e35efa54aed1b20b5c5953631290cbe4468
ludo <ludo@081d56c4-be80-0410-83e6-e326f4057db8>
2007-11-06 00:12:40 +0000

    fq path

--- samp/sampoptions/src/org/opensolaris/webstack/settings/scripts/start.sh
+++ samp/sampoptions/src/org/opensolaris/webstack/settings/scripts/start.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-/usr/sbin/svcadm -v enable svc:/network/http:apache22
+/usr/sbin/svcadm -v enable svc:/network/http:apache2
 /usr/sbin/svcadm -v enable svc:/application/database/mysql:version_50
 
 sleep 3
--- samp/sampoptions/src/org/opensolaris/webstack/settings/scripts/stop.sh
+++ samp/sampoptions/src/org/opensolaris/webstack/settings/scripts/stop.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-/usr/sbin/svcadm -v disable svc:/network/http:apache22
+/usr/sbin/svcadm -v disable svc:/network/http:apache2
 /usr/sbin/svcadm -v disable svc:/application/database/mysql:version_50
 sleep 3
