74a8c50a4337b8c8947238528749ea834f900ba2
ludo <ludo@081d56c4-be80-0410-83e6-e326f4057db8>
2007-11-12 23:48:27 +0000

    file perm

--- samp/sampoptions/src/org/opensolaris/webstack/settings/scripts/initializeasroot.sh
+++ samp/sampoptions/src/org/opensolaris/webstack/settings/scripts/initializeasroot.sh
@@ -6,4 +6,9 @@ USERNAME=$1
 echo "adding apache22 and mysql privileges to user ${USERNAME}"
 usermod -P 'Apache 22 Administration','MySql 5 Administration' ${USERNAME}
 
+
+setfacl -m user:${USERNAME}:rw- /etc/apache2/2.2/httpd.conf
+setfacl -m mask:rw- /etc/apache2/2.2/httpd.conf     
+setfacl -m user:${USERNAME}:rw- /etc/php5/5.2.4/php.ini 
+setfacl -m mask:rw- /etc/php5/5.2.4/php.ini   
 exit 0
