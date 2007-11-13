824a52f976eb3ca480f336db85ff66cad83b8b5f
ludo <ludo@081d56c4-be80-0410-83e6-e326f4057db8>
2007-11-13 00:19:27 +0000

    root-non root

--- trunk/samp/sampoptions/src/org/opensolaris/webstack/settings/scripts/initializeasroot.sh
+++ trunk/samp/sampoptions/src/org/opensolaris/webstack/settings/scripts/initializeasroot.sh
@@ -11,4 +11,6 @@ setfacl -m user:${USERNAME}:rw- /etc/apache2/2.2/httpd.conf
 setfacl -m mask:rw- /etc/apache2/2.2/httpd.conf     
 setfacl -m user:${USERNAME}:rw- /etc/php5/5.2.4/php.ini 
 setfacl -m mask:rw- /etc/php5/5.2.4/php.ini   
+setfacl -m user:${USERNAME}:rw- /etc/mysql/5.0/my.cnf 
+setfacl -m mask:rw- /etc/mysql/5.0/my.cnf   
 exit 0
