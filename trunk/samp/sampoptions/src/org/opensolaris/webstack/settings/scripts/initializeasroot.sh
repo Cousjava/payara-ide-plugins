17435093131d999822b6ecfda0e7229e65092e34
ludo <ludo@081d56c4-be80-0410-83e6-e326f4057db8>
2007-11-28 16:57:17 +0000

    mysql smf

--- trunk/samp/sampoptions/src/org/opensolaris/webstack/settings/scripts/initializeasroot.sh
+++ trunk/samp/sampoptions/src/org/opensolaris/webstack/settings/scripts/initializeasroot.sh
@@ -13,5 +13,28 @@ setfacl -m mask:rw- /etc/apache2/2.2/httpd.conf
 setfacl -m user:${USERNAME}:rw- /etc/php5/5.2.4/php.ini 
 setfacl -m mask:rw- /etc/php5/5.2.4/php.ini   
 setfacl -m user:${USERNAME}:rw- /etc/mysql/5.0/my.cnf 
-setfacl -m mask:rw- /etc/mysql/5.0/my.cnf   
+setfacl -m mask:rw- /etc/mysql/5.0/my.cnf 
+setfacl -m user:${USERNAME}:rwx /var/apache2/2.2/htdocs 
+setfacl -m mask:rwx  /var/apache2/2.2/htdocs
+
+A=`fgrep solaris.smf.manage.mysql /etc/security/prof_attr`
+if [ "$?" -ne "0"  ]; then
+	echo "MySql 5 Administration::::auths=solaris.smf.value.mysql,solaris.smf.manage.mysql" >>/etc/security/prof_attr
+else
+        echo "mysql smf profile already done"
+
+fi
+
+B=`fgrep solaris.smf.manage.mysql /etc/security/auth_attr`
+if [ "$?" -ne "0"  ]; then
+	echo 'solaris.smf.manage.mysql:::aa::' >>/etc/security/auth_attr
+else
+        echo "mysql smf auth already done"
+
+fi
+
+svccfg -s mysql setprop general/action_authorization=astring: 'solaris.smf.manage.mysql'
+svccfg -s mysql setprop general/value_authorization=astring:  'solaris.smf.manage.mysql'
+svcadm refresh mysql
+  
 exit 0
