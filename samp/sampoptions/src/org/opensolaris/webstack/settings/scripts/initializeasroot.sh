4376bd0a640f7db603466cb1bfecb1c664687179
ludo <ludo@081d56c4-be80-0410-83e6-e326f4057db8>
2007-11-29 20:14:34 +0000

    test for mysql user/group

--- samp/sampoptions/src/org/opensolaris/webstack/settings/scripts/initializeasroot.sh
+++ samp/sampoptions/src/org/opensolaris/webstack/settings/scripts/initializeasroot.sh
@@ -3,6 +3,10 @@
 ROOT_PATH=""
 USERNAME=$1
 
+echo "making sure that the mysql user and group exists, and owns /var/mysql content"
+/usr/sbin/groupadd mysql
+/usr/sbin/useradd -g mysql mysql
+chown -R mysql:mysql /var/mysql
 
 echo "allowing RW access to httpd.conf, php.ini and my.cnf to user ${USERNAME}"
 
@@ -11,7 +15,7 @@ setfacl -m mask:rw- /etc/apache2/2.2/httpd.conf
 setfacl -m user:${USERNAME}:rw- /etc/php5/5.2.4/php.ini 
 setfacl -m mask:rw- /etc/php5/5.2.4/php.ini   
 setfacl -m user:${USERNAME}:rw- /etc/php5/5.2.4/conf.d/xdebug.ini 
-setfacl -m mask:rw- /etc/php5/5.2.4/conf.d/xdebug.inim th   
+setfacl -m mask:rw- /etc/php5/5.2.4/conf.d/xdebug.ini 
 setfacl -m user:${USERNAME}:rw- /etc/mysql/5.0/my.cnf 
 setfacl -m mask:rw- /etc/mysql/5.0/my.cnf 
 setfacl -m user:${USERNAME}:rwx /var/apache2/2.2/htdocs 
@@ -21,15 +25,15 @@ A=`fgrep solaris.smf.manage.mysql /etc/security/prof_attr`
 if [ "$?" -ne "0"  ]; then
 	echo "MySql 5 Administration::::auths=solaris.smf.value.mysql,solaris.smf.manage.mysql" >>/etc/security/prof_attr
 else
-        echo "mysql smf profile already done"
+        echo "mysql smf profile already exists"
 
 fi
 
 B=`fgrep solaris.smf.manage.mysql /etc/security/auth_attr`
 if [ "$?" -ne "0"  ]; then
-	echo 'solaris.smf.manage.mysql:::aa::' >>/etc/security/auth_attr
+	echo 'solaris.smf.manage.mysql:::webstack::' >>/etc/security/auth_attr
 else
-        echo "mysql smf auth already done"
+        echo "mysql smf auth already exists"
 
 fi
 
