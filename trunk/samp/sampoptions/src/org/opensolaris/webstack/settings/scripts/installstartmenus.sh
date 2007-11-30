c33b5d07adb5e21679246b3e97456c1a22a57ee7
ludo <ludo@081d56c4-be80-0410-83e6-e326f4057db8>
2007-11-30 05:15:19 +0000

    legal stuff

--- trunk/samp/sampoptions/src/org/opensolaris/webstack/settings/scripts/installstartmenus.sh
+++ trunk/samp/sampoptions/src/org/opensolaris/webstack/settings/scripts/installstartmenus.sh
@@ -1,14 +1,28 @@
 #! /bin/sh
-
-# Usage:
 #
-#     main.sh [ -R  alternate_root ]
 #
-#   where:
-#     alternate_root is usually passed in from the Solaris installer
+# CDDL HEADER START
 #
-# Products get installed in <alternate_root>/opt/ so that when Solaris
-# is rebooted, they will be in /opt
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License (the "License").
+# You may not use this file except in compliance with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+# Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
 
 CMD=$0
 PATH_TO_CMD=`/usr/bin/dirname ${CMD}`
