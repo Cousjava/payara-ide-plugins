67e3a5c0975646357e35219354b0d8b90faeac28
ludo <ludo@081d56c4-be80-0410-83e6-e326f4057db8>
2007-11-30 01:26:44 +0000

    better menus

--- trunk/samp/sampoptions/src/org/opensolaris/webstack/settings/tray/actions/StopAction.java
+++ trunk/samp/sampoptions/src/org/opensolaris/webstack/settings/tray/actions/StopAction.java
@@ -1,29 +1,28 @@
 /*
-* CDDL HEADER START
-*
-* The contents of this file are subject to the terms of the
-* Common Development and Distribution License, Version 1.0 only
-* (the "License").  You may not use this file except in compliance
-* with the License.
-*
-* You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
-* or http://www.opensolaris.org/os/licensing.
-* See the License for the specific language governing permissions
-* and limitations under the License.
-*
-* When distributing Covered Code, include this CDDL HEADER in each
-* file and include the License file at usr/src/OPENSOLARIS.LICENSE.
-* If applicable, add the following below this CDDL HEADER, with the
-* fields enclosed by brackets "[]" replaced with your own identifying
-* information: Portions Copyright [yyyy] [name of copyright owner]
-*
-* CDDL HEADER END
-*/
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
 /*
  * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
-* Use is subject to license terms.
-*/
-
+ * Use is subject to license terms.
+ */
 package org.opensolaris.webstack.settings.tray.actions;
 
 import java.awt.MenuItem;
@@ -31,13 +30,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import org.opensolaris.webstack.settings.execution.ServerStatus;
 import org.opensolaris.webstack.settings.execution.ServersManager;
-import org.opensolaris.webstack.settings.tray.Main;
 import org.opensolaris.webstack.settings.tray.Tray;
-
 
 /**
  *
- * @author root
+ * @author Ludo
  */
 public class StopAction extends MenuItem {
 
@@ -58,9 +55,10 @@ public class StopAction extends MenuItem {
         if (running.apacheRunning && running.mySqlRunning) {
             return true;
 
-        } else if ((running.apacheRunning==false) && (running.mySqlRunning==false)) {
+        } else if ((running.apacheRunning == false) && (running.mySqlRunning == false)) {
             return false;
         } else {
             return true;
-        }    }
+        }
+    }
 }
