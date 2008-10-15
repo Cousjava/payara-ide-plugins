e0148c7e2af78f7d43597ed456a1d2e58da8a254
ludo <ludo@081d56c4-be80-0410-83e6-e326f4057db8>
2008-10-15 17:45:08 +0000

    create index.jsp

---
+++ trunk/eclipse-support/plugins/com.sun.enterprise.jst.server.sunappsrv/src/com/sun/enterprise/jst/server/sunappsrv/web/IndexJSPCreate.java
@@ -0,0 +1,166 @@
+// <editor-fold defaultstate="collapsed" desc="CDDL Licence">
+/*
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the "License").  You may not use this file except
+ * in compliance with the License.
+ *
+ * You can obtain a copy of the license at
+ * glassfishplugins/www/license/CDDLv1.0.txt or
+ * https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * HEADER in each file and include the License file at
+ * glassfishplugins/www/license/CDDLv1.0.txt.  If applicable,
+ * add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your
+ * own identifying information: Portions Copyright [yyyy]
+ * [name of copyright owner]
+ */
+// </editor-fold>
+package com.sun.enterprise.jst.server.sunappsrv.web;
+
+import java.io.ByteArrayInputStream;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jst.server.core.FacetUtil;
+import org.eclipse.ui.IEditorDescriptor;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.wst.common.componentcore.ComponentCore;
+import org.eclipse.wst.common.componentcore.datamodel.properties.IFacetDataModelProperties;
+import org.eclipse.wst.common.componentcore.datamodel.properties.IFacetProjectCreationDataModelProperties;
+import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
+import org.eclipse.wst.common.frameworks.datamodel.AbstractDataModelOperation;
+import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
+import org.eclipse.wst.common.project.facet.core.runtime.IRuntime;
+
+import com.sun.enterprise.jst.server.sunappsrv.SunAppSrvPlugin;
+
+public class IndexJSPCreate extends AbstractDataModelOperation  {
+
+
+	public IndexJSPCreate(IDataModel model) {
+		super(model);
+	}
+
+	public IStatus execute(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
+
+		IVirtualComponent comp = ComponentCore.createComponent(getProject());
+		IFile ifile = getIndexJSP(comp);
+		createIndexJSP(ifile);
+		
+		IWorkbenchWindow window=PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+	    if (window==null){
+	    	return Status.OK_STATUS;
+	    }		
+		IWorkbenchPage page = window.getActivePage();
+	    if (page==null){
+	    	return Status.OK_STATUS;
+	    }
+	    IEditorDescriptor desc = PlatformUI.getWorkbench().
+		        getEditorRegistry().getDefaultEditor(ifile.getName());
+		try {
+			page.openEditor(new FileEditorInput(ifile), desc.getId());
+		} catch (PartInitException e) {
+			// TODO Auto-generated catch block
+			SunAppSrvPlugin.logMessage("error opening index.jsp ",e);
+		}		
+		return Status.OK_STATUS;
+	}
+
+
+
+	public static IFile getIndexJSP(IVirtualComponent comp) {
+		IPath i = comp.getRootFolder().getUnderlyingFolder()
+		.getProjectRelativePath().append("index.jsp");
+		return comp.getProject().getFile(i);
+	}
+
+
+	private  void createIndexJSP(IFile index) {
+
+		ByteArrayInputStream is=null;
+		try {
+
+			String moduleName = model.getStringProperty(IFacetDataModelProperties.FACET_PROJECT_NAME);
+			is = new ByteArrayInputStream(getIndexJSPContent(moduleName).getBytes());
+
+			index.create(is, false, null);
+
+		} catch (Exception e) {
+
+		} finally {
+			try {
+				if (is!=null)
+					is.close();
+			} catch (Exception e) {
+				// do nothing
+			}
+		}
+
+	}
+
+	private String getIndexJSPContent(String contextRoot){
+		return 
+
+		"<%@page contentType=\"text/html\" pageEncoding=\"UTF-8\"%>\n"+
+		"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"\n"+
+		"    	               \"http://www.w3.org/TR/html4/loose.dtd\">\n"+
+		"\n"+
+		"<html>\n"+
+		"  <head>\n"+
+		"    	<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n"+
+		"    	<title>GlassFish JSP Page</title>\n"+
+		"  </head>\n"+
+		"  <body>\n"+
+		"    <h1>Hello World!</h1>\n"+
+		"  </body>\n"+
+		"</html> \n";
+
+
+	}
+
+
+
+	public IStatus redo(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
+		return null;
+	}
+
+
+	public IStatus undo(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
+		return null;
+	}
+
+
+
+
+	public org.eclipse.wst.server.core.IRuntime getRuntime() {
+		IRuntime runtime = (IRuntime) model.getProperty(IFacetProjectCreationDataModelProperties.FACET_RUNTIME);
+		if (runtime != null) {
+			return FacetUtil.getRuntime(runtime);
+		}
+		return null;
+	}
+
+	public IProject getProject() {
+		String projectName = model.getProperty(IFacetDataModelProperties.FACET_PROJECT_NAME).toString();
+		if (projectName != null) {
+			return ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+		}
+		return null;
+	}   
+
+}
