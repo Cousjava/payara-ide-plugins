57cbc3b3c3adad556298d1f315a099436270bb12
ludo <ludo@081d56c4-be80-0410-83e6-e326f4057db8>
2008-10-17 01:17:39 +0000

    Log output preferences

---
+++ trunk/eclipse-support/plugins/com.sun.enterprise.jst.server.sunappsrv/src/com/sun/enterprise/jst/server/sunappsrv/preferences/PreferenceConstants.java
@@ -0,0 +1,39 @@
+// <editor-fold defaultstate="collapsed" desc="CDDL Licence">
+/*
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the "License").  You may not use this file except
+ * in compliance with the License.
+ *
+ * You can obtain a copy of the license at
+ * glassfishplugins/www/license/CDDLv1.0.txt or
+ * https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * HEADER in each file and include the License file at
+ * glassfishplugins/www/license/CDDLv1.0.txt.  If applicable,
+ * add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your
+ * own identifying information: Portions Copyright [yyyy]
+ * [name of copyright owner]
+ */
+// </editor-fold>
+package com.sun.enterprise.jst.server.sunappsrv.preferences;
+
+/**
+ * Constant definitions for plug-in preferences
+ * @author  Ludovic Champenois
+ */
+public class PreferenceConstants {
+
+	//public static final String P_PATH = "pathPreference";
+
+	public static final String ENABLE_LOG = "EnableLogPreference";
+
+	//public static final String P_CHOICE = "choicePreference";
+
+	//public static final String P_STRING = "stringPreference";
+	
+}
