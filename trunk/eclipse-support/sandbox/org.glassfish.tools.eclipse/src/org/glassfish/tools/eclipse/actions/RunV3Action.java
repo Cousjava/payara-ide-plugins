e0045e0acc1792afdb87e6a8b253a875006fa621
raccah <raccah@081d56c4-be80-0410-83e6-e326f4057db8>
2008-12-16 22:44:44 +0000

    CDDL+GPL header updates.

--- trunk/eclipse-support/sandbox/org.glassfish.tools.eclipse/src/org/glassfish/tools/eclipse/actions/RunV3Action.java
+++ trunk/eclipse-support/sandbox/org.glassfish.tools.eclipse/src/org/glassfish/tools/eclipse/actions/RunV3Action.java
@@ -1,7 +1,8 @@
+// <editor-fold defaultstate="collapsed" desc="CDDL+GPL License">
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
- * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
  * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -33,6 +34,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
+// </editor-fold>
 
 package org.glassfish.tools.eclipse.actions;
 
