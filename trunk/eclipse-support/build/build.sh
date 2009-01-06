4e58151f3ad11d68315ccec2969712a0bb48acb1
kohsuke <kohsuke@081d56c4-be80-0410-83e6-e326f4057db8>
2009-01-06 22:33:54 +0000

    make the script fail if one of the steps fail

--- trunk/eclipse-support/build/build.sh
+++ trunk/eclipse-support/build/build.sh
@@ -1,12 +1,11 @@
-#!/bin/sh
-
+#!/bin/bash -ex
 
 cvs  -d :pserver:anonymous@easyeclipse.cvs.sourceforge.net:/cvsroot/easyeclipse checkout -r BRANCH_P2_WIP_IN_BRANCH_ECLIPSE_34 easyeclipse/easyeclipse/org.nexb.easyeclipse.releng
 cp -f patches/download-style.xml easyeclipse/easyeclipse/org.nexb.easyeclipse.releng
 cp -f patches/package.xml easyeclipse/easyeclipse/org.nexb.easyeclipse.releng
 cp -f build.properties easyeclipse/easyeclipse/org.nexb.easyeclipse.releng
-cd easyeclipse/easyeclipse/org.nexb.easyeclipse.releng
+pushd easyeclipse/easyeclipse/org.nexb.easyeclipse.releng
 ./build.sh glassfish-distro
-cd ../../../
+popd
 
 
