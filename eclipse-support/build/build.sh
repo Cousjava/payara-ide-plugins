c851b500fc5479e46bab8163e56559dbb7695def
imeikas <imeikas@081d56c4-be80-0410-83e6-e326f4057db8>
2009-02-09 09:54:57 +0000

    Removed patches that I moved to releng trunk

--- eclipse-support/build/build.sh
+++ eclipse-support/build/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -ex
 
 cvs  -d :pserver:anonymous@easyeclipse.cvs.sourceforge.net:/cvsroot/easyeclipse checkout -r BRANCH_P2_WIP_IN_BRANCH_ECLIPSE_34 easyeclipse/easyeclipse/org.nexb.easyeclipse.releng
-cp -f patches/download-style.xml easyeclipse/easyeclipse/org.nexb.easyeclipse.releng
-cp -f patches/package.xml easyeclipse/easyeclipse/org.nexb.easyeclipse.releng
+#cp -f patches/download-style.xml easyeclipse/easyeclipse/org.nexb.easyeclipse.releng
+#cp -f patches/package.xml easyeclipse/easyeclipse/org.nexb.easyeclipse.releng
 cp -f build.properties easyeclipse/easyeclipse/org.nexb.easyeclipse.releng
 pushd easyeclipse/easyeclipse/org.nexb.easyeclipse.releng
 ./build.sh tools-bundle-for-eclipse
