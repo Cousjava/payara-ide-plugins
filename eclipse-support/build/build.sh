4e3b43cb0646e8c8b5f946dbe0717c8ddcc79c01
ludo <ludo@081d56c4-be80-0410-83e6-e326f4057db8>
2009-02-09 05:05:58 +0000

    configuration changed so we need this again...

--- eclipse-support/build/build.sh
+++ eclipse-support/build/build.sh
@@ -2,8 +2,7 @@
 
 cvs  -d :pserver:anonymous@easyeclipse.cvs.sourceforge.net:/cvsroot/easyeclipse checkout -r BRANCH_P2_WIP_IN_BRANCH_ECLIPSE_34 easyeclipse/easyeclipse/org.nexb.easyeclipse.releng
 cp -f patches/download-style.xml easyeclipse/easyeclipse/org.nexb.easyeclipse.releng
-# Latest update to releng should make this patch obsolete. Needs confirmation.
-#cp -f patches/package.xml easyeclipse/easyeclipse/org.nexb.easyeclipse.releng
+cp -f patches/package.xml easyeclipse/easyeclipse/org.nexb.easyeclipse.releng
 cp -f build.properties easyeclipse/easyeclipse/org.nexb.easyeclipse.releng
 pushd easyeclipse/easyeclipse/org.nexb.easyeclipse.releng
 ./build.sh tools-bundle-for-eclipse
