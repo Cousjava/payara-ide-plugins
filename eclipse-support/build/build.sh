3fb7de9c5cc19d89dcf040789351abb58a1581c4
imeikas <imeikas@081d56c4-be80-0410-83e6-e326f4057db8>
2009-04-17 14:14:43 +0000

    Wrote the EasyEclipse tag for v1 in build.sh comments

--- eclipse-support/build/build.sh
+++ eclipse-support/build/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -ex
 
+# for Glassfish v1.0 use tag T_BRANCH_P2_WIP_IN_BRANCH_ECLIPSE_34_GLASSFISH_V1
+
 cvs  -d :pserver:anonymous@easyeclipse.cvs.sourceforge.net:/cvsroot/easyeclipse checkout -r BRANCH_P2_WIP_IN_BRANCH_ECLIPSE_34 easyeclipse/easyeclipse/org.nexb.easyeclipse.releng
 #cp -f patches/download-style.xml easyeclipse/easyeclipse/org.nexb.easyeclipse.releng
 #cp -f patches/package.xml easyeclipse/easyeclipse/org.nexb.easyeclipse.releng
