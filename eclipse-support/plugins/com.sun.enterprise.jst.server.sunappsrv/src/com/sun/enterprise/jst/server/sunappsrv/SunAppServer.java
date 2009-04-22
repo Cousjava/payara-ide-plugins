a5db27d77facf84ab652dc97b9f69e5cce972893
ludo <ludo@081d56c4-be80-0410-83e6-e326f4057db8>
2009-04-22 18:10:54 +0000

    New v3 PROMOTED has a different domain.xml structure for Grizzly config

--- eclipse-support/plugins/com.sun.enterprise.jst.server.sunappsrv/src/com/sun/enterprise/jst/server/sunappsrv/SunAppServer.java
+++ eclipse-support/plugins/com.sun.enterprise.jst.server.sunappsrv/src/com/sun/enterprise/jst/server/sunappsrv/SunAppServer.java
@@ -688,7 +688,42 @@ public class SunAppServer extends GenericServer {
                     }
                 }
             }));
-            
+                        
+            //New grizzly config for latest GF v3 builds (after build 44)
+            pathList.add(new TreeParser.Path("/domain/configs/config/network-config/network-listeners/network-listener",
+                                new TreeParser.NodeReader() {
+                            @Override
+                            public void readAttributes(String qname, Attributes attributes) throws SAXException {
+/*
+         <network-listeners>
+          <thread-pool max-thread-pool-size="20" min-thread-pool-size="2" thread-pool-id="http-thread-pool" max-queue-size="4096"></thread-pool>
+          <network-listener port="8080" protocol="http-listener-1" transport="tcp" name="http-listener-1" thread-pool="http-thread-pool"></network-listener>
+          <network-listener port="8181" enabled="false" protocol="http-listener-2" transport="tcp" name="http-listener-2" thread-pool="http-thread-pool"></network-listener>
+          <network-listener port="4848" protocol="admin-listener" transport="tcp" name="admin-listener" thread-pool="http-thread-pool"></network-listener>
+        </network-listeners>
+ */
+                                try {
+                                    String id = attributes.getValue("name");
+                                    if(id != null && id.length() > 0) {
+                                        int port = Integer.parseInt(attributes.getValue("port"));
+                                        boolean secure = "true".equals(attributes.getValue("security-enabled"));
+                                        boolean enabled = !"false".equals(attributes.getValue("enabled"));
+                                        if(enabled) {
+                                            HttpData data = new HttpData(id, port, secure);
+                                            SunAppSrvPlugin.logMessage( " Adding " + data);
+                                            httpMap.put(id, data);
+                                        } else {
+                                        	SunAppSrvPlugin.logMessage ("http-listener " + id + " is not enabled and won't be used.");
+                                        }
+                                    } else {
+                                    	SunAppSrvPlugin.logMessage( "http-listener found with no name");
+                                    }
+                                } catch(NumberFormatException ex) {
+                                    throw new SAXException(ex);
+                                }
+                            }
+                        }));
+           
             try {
                 TreeParser.readXml(domainXml, pathList);
                 
