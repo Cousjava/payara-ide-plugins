29fa1c52efb57a7a7d8355d42bf8884c32e99775
antonsafonov <antonsafonov@081d56c4-be80-0410-83e6-e326f4057db8>
2009-10-09 23:04:01 +0000

    made property tester safer

--- trunk/eclipse-support/plugins/com.sun.enterprise.jst.server.sunappsrv/src/com/sun/enterprise/jst/server/sunappsrv/v3/wizards/GFv3RuntimeTester.java
+++ trunk/eclipse-support/plugins/com.sun.enterprise.jst.server.sunappsrv/src/com/sun/enterprise/jst/server/sunappsrv/v3/wizards/GFv3RuntimeTester.java
@@ -4,21 +4,30 @@ import org.eclipse.core.expressions.PropertyTester;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 
+import com.sun.enterprise.jst.server.sunappsrv.SunAppSrvPlugin;
+
 public class GFv3RuntimeTester extends PropertyTester {
 
 	@Override
 	public boolean test(Object receiver, String property, Object[] args,
 			Object expectedValue) {
-		final IProject pj = ((IResource) receiver).getProject();
+		try {
+			final IProject pj = ((IResource) receiver).getProject();
 
-		if (pj == null) {
-			return false;
+			if (pj == null) {
+				return false;
+			}
+
+			boolean expected = expectedValue instanceof Boolean ? ((Boolean) expectedValue)
+					.booleanValue()
+					: Boolean.parseBoolean((String) expectedValue);
+
+			return WizardUtil.isWebOrEJBProjectWithGF3Runtime(pj) == expected;
+		} catch (Exception e) {
+			SunAppSrvPlugin.logMessage(
+					"failed to check GFv3 enablement property", e);
 		}
-
-		boolean expected =  expectedValue instanceof Boolean ? ((Boolean) expectedValue).booleanValue() :
-			Boolean.parseBoolean((String) expectedValue);
-		
-		return WizardUtil.isWebOrEJBProjectWithGF3Runtime(pj) == expected;
+		return false;
 	}
 
 }
