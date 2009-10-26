43a20b2799b6e7fff9b515e2e3b6d897a0560560
antonsafonov <antonsafonov@081d56c4-be80-0410-83e6-e326f4057db8>
2009-10-26 12:51:47 +0000

    fixed new servlet model to work in Eclipse 3.4.2 + wtp 3.0.4

--- trunk/eclipse-support/plugins/com.sun.enterprise.jst.server.sunappsrv/src/com/sun/enterprise/jst/server/sunappsrv/v3/Web30MergedModelProviderFactory.java
+++ trunk/eclipse-support/plugins/com.sun.enterprise.jst.server.sunappsrv/src/com/sun/enterprise/jst/server/sunappsrv/v3/Web30MergedModelProviderFactory.java
@@ -37,19 +37,41 @@
 // </editor-fold>
 package com.sun.enterprise.jst.server.sunappsrv.v3;
 
+import java.util.HashMap;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.jst.j2ee.model.IModelProvider;
-import org.eclipse.jst.j2ee.model.IModelProviderFactory;
+import org.eclipse.jst.jee.model.internal.Web25MergedModelProviderFactory;
 import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
 
-public class Web30ModelProviderFactory implements IModelProviderFactory {
+import com.sun.enterprise.jst.server.sunappsrv.v3.wizards.WizardUtil;
+
+public class Web30MergedModelProviderFactory extends Web25MergedModelProviderFactory {
+
+	private HashMap<IProject, IModelProvider> xmlResources = new HashMap<IProject, IModelProvider>();
 
 	public IModelProvider create(IProject project) {
-		return new Web30ModelProvider(project);
+		if (WizardUtil.isWebProjectWithGF3Runtime(project)) {
+			IModelProvider result = getResource(project);
+			if (result == null || ((Web30ModelProvider) result).isDisposed()) {
+				result = new Web30ModelProvider(project);
+				addResource(project, result);
+			}
+			return result;
+		} else {
+			return super.create(project);
+		}
 	}
 
 	public IModelProvider create(IVirtualComponent component) {
-		return new Web30ModelProvider(component.getProject());
+		return create(component.getProject());
 	}
 
+	private void addResource(IProject project, IModelProvider modelProvider){
+		xmlResources.put(project, modelProvider);
+	}
+
+	private IModelProvider getResource(IProject project){
+		return xmlResources.get(project);
+	}
 }
