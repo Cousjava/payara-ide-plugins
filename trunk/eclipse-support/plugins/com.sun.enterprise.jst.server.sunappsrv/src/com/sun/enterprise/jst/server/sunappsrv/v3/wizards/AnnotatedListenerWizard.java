7ce8db3c7de19598a866b37b208fd54993c0378b
raccah <raccah@081d56c4-be80-0410-83e6-e326f4057db8>
2009-10-28 21:36:42 +0000

    Cleanup annotations and comment in 3.4.x variation of generation code.

--- trunk/eclipse-support/plugins/com.sun.enterprise.jst.server.sunappsrv/src/com/sun/enterprise/jst/server/sunappsrv/v3/wizards/AnnotatedListenerWizard.java
+++ trunk/eclipse-support/plugins/com.sun.enterprise.jst.server.sunappsrv/src/com/sun/enterprise/jst/server/sunappsrv/v3/wizards/AnnotatedListenerWizard.java
@@ -90,10 +90,9 @@ public class AnnotatedListenerWizard extends AddListenerWizard {
 					protected NewJavaEEArtifactClassOperation getNewClassOperation() {
 						return new NewListenerClassOperation(model) {
 							// this method is the eclipse 3.5 way of doing things
-							// we can't use super or officially "override" with the annotation because
-							// the method doesn't exist in the 3.4.x code
-							//@Override
-							@SuppressWarnings("unused")
+							// we can't use super because the method doesn't exist
+							// in the older 3.4 code
+							@Override
 							protected String generateTemplateSource(CreateJavaEEArtifactTemplateModel templateModel, Object templateImpl) 
 								throws JETException {
 								String superSource = CodeGenerationUtils.get35SuperTemplateSource(templateModel, templateImpl);
