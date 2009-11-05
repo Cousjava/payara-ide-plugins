b94227a2b565f00e703aaae6afc4798a7a445be2
raccah <raccah@081d56c4-be80-0410-83e6-e326f4057db8>
2009-11-05 21:57:34 +0000

    Cleanup annotations and comment in 3.4.x variation of generation code.

--- trunk/eclipse-support/plugins/com.sun.enterprise.jst.server.sunappsrv/src/com/sun/enterprise/jst/server/sunappsrv/v3/wizards/JavaEE6SessionBeanWizard.java
+++ trunk/eclipse-support/plugins/com.sun.enterprise.jst.server.sunappsrv/src/com/sun/enterprise/jst/server/sunappsrv/v3/wizards/JavaEE6SessionBeanWizard.java
@@ -319,10 +319,9 @@ public class JavaEE6SessionBeanWizard extends AddSessionBeanWizard {
 							}
 
 							// this method is the eclipse 3.5 way of doing things
-							// we can't use super or officially "override" with the annotation because
-							// the method doesn't exist in the 3.4.x code
-							//@Override
-							@SuppressWarnings("unused")
+							// we can't use super because the method doesn't exist
+							// in the older 3.4 code
+							@Override
 							protected String generateTemplateSource(CreateJavaEEArtifactTemplateModel templateModel, Object templateImpl) 
 								throws JETException {
 					            try {
