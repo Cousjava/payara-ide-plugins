a9cdad2da43343dd8c6b90e524ade2fd31dbf461
raccah <raccah@081d56c4-be80-0410-83e6-e326f4057db8>
2009-12-09 23:22:01 +0000

    Fix for issue 281 - better handling of v2 UC launch errors.

--- eclipse-support/plugins/com.sun.enterprise.jst.server.sunappsrv/src/com/sun/enterprise/jst/server/sunappsrv/actions/PreludeUpdateCenterAction.java
+++ eclipse-support/plugins/com.sun.enterprise.jst.server.sunappsrv/src/com/sun/enterprise/jst/server/sunappsrv/actions/PreludeUpdateCenterAction.java
@@ -39,6 +39,7 @@
 package com.sun.enterprise.jst.server.sunappsrv.actions;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.apache.tools.ant.taskdefs.Execute;
 import org.eclipse.wst.server.core.IServer;
@@ -73,10 +74,35 @@ public class PreludeUpdateCenterAction extends OpenBrowserEditorAction {
     		String[] command = new String[]{
     				loc    		}; 
     		try {
-    			// TODO - show an error if !File.exists?
-    			Execute.launch(null, command, null, new File(sab.getSunApplicationServerInstallationDirectory()), true);
+    			File appinstallDir = new File(sab.getSunApplicationServerInstallationDirectory());
+ 
+    			// need to test this separately because in this case the launch below doesn't work, but
+    			// the exit code is not a failure
+    			if (!appinstallDir.canWrite()) {
+    				throw new IOException("Cannot write to directory " + appinstallDir);
+    			}
+
+    			Process process = Execute.launch(null, command, null, appinstallDir, true);
+    			try {
+    				int exitValue = process.exitValue();
+
+    				// this test works in debug with breakpoints, but not regular run for certain cases - could
+    				// be a timing issue because same "failure" case exits with 1 or 0 depending on timing
+    				if (Execute.isFailure(exitValue)) {
+    					// can get fancy here and try to get info out of the process error stream
+    					// on why it failed, but it is not straightforward and this is a corner case
+    					throw new IOException();
+    				}
+    			} catch (IllegalThreadStateException e) {
+    				// if it gets here, the ui came up and is still running, we don't want to 
+    				// block - this is actually the "success" case
+    			}
     			return;
     		} catch (Exception ioe) {
+    			String exceptionMessage = ioe.getMessage();
+    			String message = ((exceptionMessage != null) ? "Error launching updatetool executable: " + exceptionMessage :
+    				"Error launching updatetool executable");
+    			showMessageDialog(message);
     			SunAppSrvPlugin.logMessage("error Launching Executable", ioe);
     		}
     	} else { // V3
