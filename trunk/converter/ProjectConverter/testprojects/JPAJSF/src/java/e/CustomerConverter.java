eaa671ff90de268aaaa8ca0ef82c07f89654cd95
vbkraemer <vbkraemer@081d56c4-be80-0410-83e6-e326f4057db8>
2010-03-25 19:23:37 +0000

    initial commit

---
+++ trunk/converter/ProjectConverter/testprojects/JPAJSF/src/java/e/CustomerConverter.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package e;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
+/**
+ *
+ * @author vkraemer
+ */
+public class CustomerConverter implements Converter {
+
+    public Object getAsObject(FacesContext facesContext, UIComponent component, String string) {
+        if (string == null || string.length() == 0) {
+            return null;
+        }
+        Integer id = new Integer(string);
+        CustomerJpaController controller = (CustomerJpaController) facesContext.getApplication().getELResolver().getValue(facesContext.getELContext(), null, "customerJpa");
+        return controller.findCustomer(id);
+    }
+
+    public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
+        if (object == null) {
+            return null;
+        }
+        if (object instanceof Customer) {
+            Customer o = (Customer) object;
+            return o.getCustomerId() == null ? "" : o.getCustomerId().toString();
+        } else {
+            throw new IllegalArgumentException("object " + object + " is of type " + object.getClass().getName() + "; expected type: e.Customer");
+        }
+    }
+
+}
