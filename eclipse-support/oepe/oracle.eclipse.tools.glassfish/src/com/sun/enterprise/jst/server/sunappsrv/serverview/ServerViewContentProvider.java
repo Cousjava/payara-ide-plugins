fc474a526636fa9910d22bcfa84dd4c918731c57
ludo <ludo@081d56c4-be80-0410-83e6-e326f4057db8>
2011-02-17 20:50:07 +0000

    Server View management tree, similar to the Netbeans runtime tab for GlassFish 3.1 only

---
+++ eclipse-support/oepe/oracle.eclipse.tools.glassfish/src/com/sun/enterprise/jst/server/sunappsrv/serverview/ServerViewContentProvider.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2011 Oracle and/or its affiliates. All rights reserved.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Oracle
+ */
+
+package com.sun.enterprise.jst.server.sunappsrv.serverview;
+
+
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.wst.server.core.IServer;
+import org.eclipse.wst.server.ui.internal.viewers.BaseContentProvider;
+
+import com.sun.enterprise.jst.server.sunappsrv.SunAppServer;
+
+@SuppressWarnings("restriction")
+public class ServerViewContentProvider extends BaseContentProvider implements ITreeContentProvider{
+
+	
+	public ServerViewContentProvider() {
+	}
+
+	public Object[] getChildren(Object parentElement) {
+		if( parentElement instanceof IServer ){
+			IServer server = (IServer) parentElement;
+			
+			//only active for glassfish 3.1 server which is started!!!
+			String s  = server.getRuntime().getRuntimeType().getId();
+			if((s.equals("org.glassfish.jst.server.runtime.glassfish31") &&( server.getServerState()==IServer.STATE_STARTED))){
+				SunAppServer ser = (SunAppServer)server.loadAdapter(SunAppServer.class, new NullProgressMonitor() );
+				if( ser!=null ){
+					TreeNode root = new TreeNode("GlassFish Management", "GlassFish Management", null);
+					//Applications Node
+					DeployedApplicationsNode apps = new DeployedApplicationsNode(server );
+					//Resources Node
+					DeployedWebServicesNode ws = new DeployedWebServicesNode(server );
+
+					ResourcesNode rs = new ResourcesNode("Resources",NodeTypes.RESOURCES,server, null);
+					rs.setContainerNode();
+					root.addChild(apps);
+					root.addChild(rs);
+					root.addChild(ws);
+					return new Object[]{root};
+				}
+			}
+		}if( parentElement instanceof TreeNode ){
+			TreeNode root = (TreeNode )parentElement;
+			return root.getChildren();
+		}
+		return null;
+	}
+
+	public Object[] getElements(Object parentElement) {
+		return getChildren(parentElement);
+	}
+	
+	
+	public Object getParent(Object element) {
+		if( element instanceof DeployedApplicationsNode){
+			return ((DeployedApplicationsNode)element).getServer();
+		}else if( element instanceof ApplicationNode ){
+			return ((ApplicationNode)element).getParent();
+		}else if( element instanceof TreeNode ){
+			TreeNode m  = (TreeNode)element;
+			return m.getParent();
+		}	
+		return null;
+	}
+
+	public boolean hasChildren(Object element) {
+		if( element instanceof IServer ){
+			return true;
+		}else if( element instanceof DeployedApplicationsNode ){
+			return true;
+		}else if( element instanceof ApplicationNode ){
+			return true;
+		}else if( element instanceof TreeNode ){
+			TreeNode m  = (TreeNode)element;
+			return m.getChildren().length>0 ;
+		}
+		return false;
+	}
+
+	
+
+	
+}
---
+++ eclipse-support/oepe/oracle.eclipse.tools.glassfish/src/com/sun/enterprise/jst/server/sunappsrv/serverview/actions/WSDLInfoWebServiceAction.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2011 Oracle and/or its affiliates. All rights reserved.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Oracle
+ */
+package com.sun.enterprise.jst.server.sunappsrv.serverview.actions;
+
+import java.net.URL;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.browser.IWebBrowser;
+import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
+
+import com.sun.enterprise.jst.server.sunappsrv.SunAppServer;
+import com.sun.enterprise.jst.server.sunappsrv.SunAppServerBehaviour;
+import com.sun.enterprise.jst.server.sunappsrv.SunAppSrvPlugin;
+import com.sun.enterprise.jst.server.sunappsrv.commands.Utils;
+import com.sun.enterprise.jst.server.sunappsrv.serverview.DeployedWebServicesNode;
+import com.sun.enterprise.jst.server.sunappsrv.serverview.WebServiceNode;
+
+public 	class WSDLInfoWebServiceAction extends Action {
+	ISelection selection;
+
+	public WSDLInfoWebServiceAction(ISelection selection) {
+		setText("Show WDSL content in Browser");
+
+
+		this.selection = selection;
+	}
+
+	public void runWithEvent(Event event) {
+		if (selection instanceof TreeSelection) {
+			TreeSelection ts = (TreeSelection) selection;
+			Object obj = ts.getFirstElement();
+			if (obj instanceof WebServiceNode) {
+				final WebServiceNode module = (WebServiceNode) obj;
+				final DeployedWebServicesNode target = (DeployedWebServicesNode) module
+						.getParent();
+
+				try {
+					final SunAppServerBehaviour be = target
+							.getServerBehavior();
+
+					IWorkbenchBrowserSupport browserSupport = PlatformUI
+							.getWorkbench().getBrowserSupport();
+					IWebBrowser browser = browserSupport
+							.createBrowser(
+									IWorkbenchBrowserSupport.LOCATION_BAR
+											| IWorkbenchBrowserSupport.NAVIGATION_BAR,
+									null, null, null);
+					SunAppServer server= be.getSunAppServer();
+			         String host = server.getServer().getHost();
+			         int port = Integer.parseInt(server.getServerPort());
+
+			         String url = Utils.getHttpListenerProtocol(host,port)+"://"+ host+":"+port+ "/"+module.getWSInfo().getWsdlUrl();
+					browser.openURL(new URL(url));
+
+				} catch (Exception e) {
+					SunAppSrvPlugin.logMessage("Error opening browser: "
+							+ e.getMessage());
+				}
+			}
+			super.run();
+		}
+	}
+
+	@Override
+	public void run() {
+		this.runWithEvent(null);
+	}
+
+}
