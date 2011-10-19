83b5803827e8b343874aca477fe1a3c4c89a594a
vbkraemer <vbkraemer@081d56c4-be80-0410-83e6-e326f4057db8>
2011-10-19 22:22:47 +0000

    NPE reported in https://forums.oracle.com/forums/thread.jspa?threadID=2298630&tstart=0

--- eclipse-support/oepe/oracle.eclipse.tools.glassfish/src/com/sun/enterprise/jst/server/sunappsrv/InstallJAXRS.java
+++ eclipse-support/oepe/oracle.eclipse.tools.glassfish/src/com/sun/enterprise/jst/server/sunappsrv/InstallJAXRS.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -10,27 +10,22 @@
  *     Sun Microsystems
  *     Oracle
  */
-
 package com.sun.enterprise.jst.server.sunappsrv;
 
-	import org.eclipse.core.runtime.CoreException;
-	import org.eclipse.core.runtime.IProgressMonitor;
-	import org.eclipse.jst.common.project.facet.core.libprov.LibraryProviderOperation;
-	import org.eclipse.jst.common.project.facet.core.libprov.LibraryProviderOperationConfig;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jst.common.project.facet.core.libprov.LibraryProviderOperation;
+import org.eclipse.jst.common.project.facet.core.libprov.LibraryProviderOperationConfig;
 
-	/**
-	 * @author ludo>
-	 */
+/**
+ * @author ludo>
+ */
+public class InstallJAXRS extends LibraryProviderOperation {
 
-	public class InstallJAXRS   extends LibraryProviderOperation{
-	    public void execute( final LibraryProviderOperationConfig config,
-	                         final IProgressMonitor monitor )
-
-	        throws CoreException{
-	        monitor.beginTask( "", 1 );
-
-	            monitor.done();
-	        }
-	    }
-	    
-	
+    public void execute(final LibraryProviderOperationConfig config,
+            final IProgressMonitor monitor)
+            throws CoreException {
+        monitor.beginTask("", 1);
+        monitor.done();
+    }
+}
