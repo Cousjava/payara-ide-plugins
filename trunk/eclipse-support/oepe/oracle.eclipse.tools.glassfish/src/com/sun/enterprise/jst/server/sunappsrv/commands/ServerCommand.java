568a44bc2df9a584a111937d1d3e75f80c55a9a6
vbkraemer <vbkraemer@081d56c4-be80-0410-83e6-e326f4057db8>
2011-10-24 17:30:27 +0000

    fix for warnings being treated as errors

--- trunk/eclipse-support/oepe/oracle.eclipse.tools.glassfish/src/com/sun/enterprise/jst/server/sunappsrv/commands/ServerCommand.java
+++ trunk/eclipse-support/oepe/oracle.eclipse.tools.glassfish/src/com/sun/enterprise/jst/server/sunappsrv/commands/ServerCommand.java
@@ -22,6 +22,7 @@ import java.net.HttpURLConnection;
 import java.net.URLDecoder;
 import java.text.MessageFormat;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.jar.Manifest;
 import java.util.logging.Level;
@@ -181,7 +182,7 @@ public abstract class ServerCommand {
         Manifest m = new Manifest();
         m.read(in);
         String outputCode = m.getMainAttributes().getValue("exit-code"); // NOI18N
-        if (outputCode.equalsIgnoreCase("Success")) { // NOI18N
+        if (null == outputCode || !"FAILURE".equals(outputCode.toUpperCase(Locale.ENGLISH))) { // NOI18N
             readManifest(m);
             result = true;
         } else {
