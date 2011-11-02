04675afc60fab01037a49f67a54539bbb6667e23
vbkraemer <vbkraemer@081d56c4-be80-0410-83e6-e326f4057db8>
2011-11-02 19:37:30 +0000

    clean up indents and other format issues.

--- trunk/eclipse-support/oepe/oracle.eclipse.tools.glassfish/src/com/sun/enterprise/jst/server/sunappsrv/serverview/ServerViewContentProvider.java
+++ trunk/eclipse-support/oepe/oracle.eclipse.tools.glassfish/src/com/sun/enterprise/jst/server/sunappsrv/serverview/ServerViewContentProvider.java
@@ -9,10 +9,7 @@
  * Contributors:
  *     Oracle
  */
-
 package com.sun.enterprise.jst.server.sunappsrv.serverview;
-
-
 
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.viewers.ITreeContentProvider;
@@ -23,84 +20,75 @@ import com.sun.enterprise.jst.server.sunappsrv.SunAppServer;
 import com.sun.enterprise.jst.server.sunappsrv.SunAppSrvPlugin;
 
 @SuppressWarnings("restriction")
-public class ServerViewContentProvider extends BaseContentProvider implements ITreeContentProvider{
+public class ServerViewContentProvider extends BaseContentProvider implements ITreeContentProvider {
 
-	
-	public ServerViewContentProvider() {
-	}
+    public ServerViewContentProvider() {
+    	return;
+    }
 
-	public Object[] getChildren(Object parentElement) {
-		if( parentElement instanceof IServer ){
-			IServer server = (IServer) parentElement;
-			
-			//only active for glassfish 3.1.x server which is started!!!
+    @Override
+    public Object[] getChildren(Object parentElement) {
+        if (parentElement instanceof IServer) {
+            IServer server = (IServer) parentElement;
+
+            //only active for glassfish 3.1.x server which is started!!!
             boolean is31x = SunAppSrvPlugin.is31OrAbove(server.getRuntime());
-			if((is31x &&( server.getServerState()==IServer.STATE_STARTED))){
-				SunAppServer ser = (SunAppServer)server.loadAdapter(SunAppServer.class, new NullProgressMonitor() );
-				
-				if( ser!=null ){
-/*					try {
-						if ((!ser.isLocalServer())&&(!Utils.isSecurePort(ser.getServer().getHost(), Integer.parseInt(ser.getAdminServerPort())))){
-								TreeNode error = new TreeNode ("Error: the GlassFish server is remote, but not secured, Eclipse cannot access it", "",null);
-								return new Object[]{error};
-					    }
-					}catch (Exception e) {
-							TreeNode error = new TreeNode ("Error: "+e.getMessage(), "",null);
-							return new Object[]{error};
-					}*/
-					TreeNode root = new TreeNode("GlassFish Management", "GlassFish Management", null);
-					//Applications Node
-					DeployedApplicationsNode apps = new DeployedApplicationsNode(server );
-					//Resources Node
-					DeployedWebServicesNode ws = new DeployedWebServicesNode(server );
+            if ((is31x && (server.getServerState() == IServer.STATE_STARTED))) {
+                SunAppServer ser = (SunAppServer) server.loadAdapter(SunAppServer.class, new NullProgressMonitor());
 
-					ResourcesNode rs = new ResourcesNode("Resources",NodeTypes.RESOURCES,server, null);
-					rs.setContainerNode();
-					root.addChild(apps);
-					root.addChild(rs);
-					root.addChild(ws);
-					return new Object[]{root};
-				}
-			}
-		}if( parentElement instanceof TreeNode ){
-			TreeNode root = (TreeNode )parentElement;
-			return root.getChildren();
-		}
-		return null;
-	}
+                if (ser != null) {
+                    TreeNode root = new TreeNode("GlassFish Management", "GlassFish Management", null);
+                    //Applications Node
+                    DeployedApplicationsNode apps = new DeployedApplicationsNode(server);
+                    //Resources Node
+                    DeployedWebServicesNode ws = new DeployedWebServicesNode(server);
 
-	public Object[] getElements(Object parentElement) {
-		return getChildren(parentElement);
-	}
-	
-	
-	public Object getParent(Object element) {
-		if( element instanceof DeployedApplicationsNode){
-			return ((DeployedApplicationsNode)element).getServer();
-		}else if( element instanceof ApplicationNode ){
-			return ((ApplicationNode)element).getParent();
-		}else if( element instanceof TreeNode ){
-			TreeNode m  = (TreeNode)element;
-			return m.getParent();
-		}	
-		return null;
-	}
+                    ResourcesNode rs = new ResourcesNode("Resources", NodeTypes.RESOURCES, server, null);
+                    rs.setContainerNode();
+                    root.addChild(apps);
+                    root.addChild(rs);
+                    root.addChild(ws);
+                    return new Object[]{root};
+                }
+            }
+        }
+        if (parentElement instanceof TreeNode) {
+            TreeNode root = (TreeNode) parentElement;
+            return root.getChildren();
+        }
+        return null;
+    }
 
-	public boolean hasChildren(Object element) {
-		if( element instanceof IServer ){
-			return true;
-		}else if( element instanceof DeployedApplicationsNode ){
-			return true;
-		}else if( element instanceof ApplicationNode ){
-			return true;
-		}else if( element instanceof TreeNode ){
-			TreeNode m  = (TreeNode)element;
-			return m.getChildren().length>0 ;
-		}
-		return false;
-	}
+    @Override
+    public Object[] getElements(Object parentElement) {
+        return getChildren(parentElement);
+    }
 
-	
+    @Override
+    public Object getParent(Object element) {
+        if (element instanceof DeployedApplicationsNode) {
+            return ((DeployedApplicationsNode) element).getServer();
+        } else if (element instanceof ApplicationNode) {
+            return ((ApplicationNode) element).getParent();
+        } else if (element instanceof TreeNode) {
+            TreeNode m = (TreeNode) element;
+            return m.getParent();
+        }
+        return null;
+    }
 
-	
+    @Override
+    public boolean hasChildren(Object element) {
+        if (element instanceof IServer) {
+            return true;
+        } else if (element instanceof DeployedApplicationsNode) {
+            return true;
+        } else if (element instanceof ApplicationNode) {
+            return true;
+        } else if (element instanceof TreeNode) {
+            TreeNode m = (TreeNode) element;
+            return m.getChildren().length > 0;
+        }
+        return false;
+    }
 }
--- trunk/eclipse-support/oepe/oracle.eclipse.tools.glassfish/src/com/sun/enterprise/jst/server/sunappsrv/serverview/ServerViewLabelProvider.java
+++ trunk/eclipse-support/oepe/oracle.eclipse.tools.glassfish/src/com/sun/enterprise/jst/server/sunappsrv/serverview/ServerViewLabelProvider.java
@@ -9,9 +9,7 @@
  * Contributors:
  *     Oracle
  */
-
 package com.sun.enterprise.jst.server.sunappsrv.serverview;
-
 
 import org.eclipse.jface.viewers.ITableFontProvider;
 import org.eclipse.jface.viewers.LabelProvider;
@@ -22,61 +20,62 @@ import com.sun.enterprise.jst.server.sunappsrv.actions.AppServerContextAction;
 
 public class ServerViewLabelProvider extends LabelProvider implements ITableFontProvider {
 
-	static Image resourceImg=AppServerContextAction.getImageDescriptorFromlocalImage("icons/obj16/resources.gif").createImage();
-	static Image serverImg=AppServerContextAction.getImageDescriptorFromlocalImage("icons/obj16/sunappsrv.gif").createImage();
-	static Image earImage=AppServerContextAction.getImageDescriptorFromlocalImage("icons/obj16/ear.gif").createImage();
-	static Image EJBImage=AppServerContextAction.getImageDescriptorFromlocalImage("icons/obj16/ejb_module.gif").createImage();
-	static Image WARImage = AppServerContextAction.getImageDescriptorFromlocalImage("icons/obj16/web_module.gif").createImage();
-	static Image WSImage=AppServerContextAction.getImageDescriptorFromlocalImage("icons/obj16/webservice.png").createImage();
-	
-	@Override
-	public String getText(Object element) {
-	//	if( element instanceof DeployedApplicationsNode ){
-	//		return "Applications label provider";
-	//	}else if( element instanceof ResourcesNode ){
-	//		return "resources label provider";
-	//	}else
-			if( element instanceof TreeNode ){
-			TreeNode module = (TreeNode)element;
-			String name = module.getName() ;
-			if( name.endsWith("/") && !name.equals("/") ){
-				name = name.substring(0,name.length()-1) ;
-			}
-			return name ;
-		}
-		return null;
-	}
+    static Image resourceImg = AppServerContextAction.getImageDescriptorFromlocalImage("icons/obj16/resources.gif").createImage();
+    static Image serverImg = AppServerContextAction.getImageDescriptorFromlocalImage("icons/obj16/sunappsrv.gif").createImage();
+    static Image earImage = AppServerContextAction.getImageDescriptorFromlocalImage("icons/obj16/ear.gif").createImage();
+    static Image EJBImage = AppServerContextAction.getImageDescriptorFromlocalImage("icons/obj16/ejb_module.gif").createImage();
+    static Image WARImage = AppServerContextAction.getImageDescriptorFromlocalImage("icons/obj16/web_module.gif").createImage();
+    static Image WSImage = AppServerContextAction.getImageDescriptorFromlocalImage("icons/obj16/webservice.png").createImage();
+    
+    public ServerViewLabelProvider() {
+    	return;
+    }
 
-	public Image getImage(Object element) {
-		 if( element instanceof ApplicationNode ){
-			 ApplicationNode module = (ApplicationNode)element;
-			if( module.getApplicationInfo().getType().equals("web"))
-				return WARImage;
-			if( module.getApplicationInfo().getType().equals("ejb"))
-				return EJBImage;
-			if( module.getApplicationInfo().getType().equals("ear"))
-				return earImage;
-		}else if (element instanceof ResourcesNode){
-			ResourcesNode rn = (ResourcesNode) element;
-			if (rn.getResource()==null){
-				return resourceImg;
-			} else {
-				return serverImg;
-			}
-		}else if (element instanceof DeployedWebServicesNode){
-			return WSImage;
-		}else if (element instanceof WebServiceNode){
-			return WSImage;
-		}
-			return serverImg ; 
-		
-	}
+    @Override
+    public String getText(Object element) {
+        if (element instanceof TreeNode) {
+            TreeNode module = (TreeNode) element;
+            String name = module.getName();
+            if (name.endsWith("/") && !name.equals("/")) {
+                name = name.substring(0, name.length() - 1);
+            }
+            return name;
+        }
+        return null;
+    }
 
-	@Override
-	public Font getFont(Object arg0, int arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Image getImage(Object element) {
+        if (element instanceof ApplicationNode) {
+            ApplicationNode module = (ApplicationNode) element;
+            if (module.getApplicationInfo().getType().equals("web")) {
+                return WARImage;
+            }
+            if (module.getApplicationInfo().getType().equals("ejb")) {
+                return EJBImage;
+            }
+            if (module.getApplicationInfo().getType().equals("ear")) {
+                return earImage;
+            }
+        } else if (element instanceof ResourcesNode) {
+            ResourcesNode rn = (ResourcesNode) element;
+            if (rn.getResource() == null) {
+                return resourceImg;
+            } else {
+                return serverImg;
+            }
+        } else if (element instanceof DeployedWebServicesNode) {
+            return WSImage;
+        } else if (element instanceof WebServiceNode) {
+            return WSImage;
+        }
+        return serverImg;
 
-	
+    }
+
+    @Override
+    public Font getFont(Object arg0, int arg1) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
