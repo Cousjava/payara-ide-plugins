909fe0b06af6d1a60778d61a499e0558e821f22b
piotrik <piotrik@081d56c4-be80-0410-83e6-e326f4057db8>
2012-07-22 15:48:34 +0000

    sdk integration

---
+++ oracle.eclipse.tools.glassfish/src/com/sun/enterprise/jst/server/sunappsrv/GlassfishV3ServerBehavior.java
@@ -0,0 +1,5 @@
+package com.sun.enterprise.jst.server.sunappsrv;
+
+public class GlassfishV3ServerBehavior extends GlassfishGenericServerBehaviour {
+
+}
---
+++ oracle.eclipse.tools.glassfish/src/com/sun/enterprise/jst/server/sunappsrv/GlassfishV4Server.java
@@ -0,0 +1,5 @@
+package com.sun.enterprise.jst.server.sunappsrv;
+
+public class GlassfishV4Server extends GlassfishGenericServer {
+
+}
