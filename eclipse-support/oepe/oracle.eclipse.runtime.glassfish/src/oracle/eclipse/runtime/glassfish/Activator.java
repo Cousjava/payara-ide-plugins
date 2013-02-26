68314ff241d871b5bb3843ccb04f64dc9d746574
piotrik <piotrik@081d56c4-be80-0410-83e6-e326f4057db8>
2013-02-26 11:55:56 +0000

    moving SDK integrated brach to trunk

---
+++ eclipse-support/oepe/oracle.eclipse.runtime.glassfish/src/oracle/eclipse/runtime/glassfish/Activator.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2011 Oracle and/or its affiliates. All rights reserved.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Oracle
+ */
+
+package oracle.eclipse.runtime.glassfish;
+
+import org.eclipse.ui.IStartup;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+public class Activator  extends AbstractUIPlugin implements IStartup {
+
+	private static BundleContext context;
+    // The shared instance
+    private static Activator plugin;
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+       Activator.context = context;
+       Activator.plugin = this;
+    }
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	       Activator.plugin = null;
+	}
+    public static Activator getDefault() {
+        return plugin;
+    }
+
+	public void earlyStartup() {
+		// TODO Auto-generated method stub
+		
+	}
+}
